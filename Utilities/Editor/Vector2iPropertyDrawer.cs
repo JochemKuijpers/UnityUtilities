c6a092046d079a151acf6683b7fc76fa0809a20a
JochemKuijpers <github.com@jochemkuijpers.nl>
2016-09-04 18:20:33 +0200

    Vector2i + PropertyDrawer (initial commit)

---
+++ Utilities/Editor/Vector2iPropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Utilities
+{
+    [CustomPropertyDrawer(typeof(Vector2i))]
+    public class Vector2iPropertyDrawer : PropertyDrawer
+    {
+        void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            SerializedProperty x = property.FindPropertyRelative("x");
+            SerializedProperty y = property.FindPropertyRelative("y");
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            Rect left = new Rect(position.position, new Vector2(position.width/2, position.height));
+            Rect right = new Rect(position.position + new Vector2(position.width / 2, 0), new Vector2(position.width / 2, position.height));
+
+            EditorGUI.PropertyField(left, x, new GUIContent("X"));
+            EditorGUI.PropertyField(right, y, new GUIContent("Y"));
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Utilities/Vector2i.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public class Vector2i
+    {
+        public int x;
+        public int y;
+
+        public int sqrMagnitude {
+            get { return x * x + y * y; }
+        }
+
+        public float magnitude
+        {
+            get { return Mathf.Sqrt(sqrMagnitude); }
+        }
+
+        public Vector2 normalized
+        {
+            get { return new Vector2(x, y).normalized; }
+        }
+
+        public Vector2i(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2i(Vector2 a)
+        {
+            this.x = (int) a.x;
+            this.y = (int) a.y;
+        }
+
+        public static int Dot(Vector2i a, Vector2i b)
+        {
+            return a.x * b.x + a.y * b.y;
+        }
+
+        public static float Dot(Vector2i a, Vector2 b)
+        {
+            return a.x * b.x + a.y * b.y;
+        }
+
+        public static float Dot(Vector2 a, Vector2i b)
+        {
+            return a.x * b.x + a.y * b.y;
+        }
+
+        public static Vector2i operator +(Vector2i a, Vector2i b)
+        {
+            return new Vector2i(a.x + b.x, a.y + b.y);
+        }
+
+        public static Vector2 operator +(Vector2i a, Vector2 b)
+        {
+            return new Vector2(a.x + b.x, a.y + b.y);
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2i b)
+        {
+            return new Vector2(a.x + b.x, a.y + b.y);
+        }
+
+        public static Vector2i operator -(Vector2i a, Vector2i b)
+        {
+            return new Vector2i(a.x - b.x, a.y - b.y);
+        }
+
+        public static Vector2 operator -(Vector2i a, Vector2 b)
+        {
+            return new Vector2(a.x - b.x, a.y - b.y);
+        }
+
+        public static Vector2 operator -(Vector2 a, Vector2i b)
+        {
+            return new Vector2(a.x - b.x, a.y - b.y);
+        }
+
+        public static Vector2i operator *(Vector2i a, int s)
+        {
+            return new Vector2i(a.x * s, a.y * s);
+        }
+
+        public static Vector2 operator *(Vector2i a, float s)
+        {
+            return new Vector2(a.x * s, a.y * s);
+        }
+
+        public static Vector2i operator *(int s, Vector2i a)
+        {
+            return new Vector2i(a.x * s, a.y * s);
+        }
+
+        public static Vector2 operator *(float s, Vector2i a)
+        {
+            return new Vector2(a.x * s, a.y * s);
+        }
+
+        public static Vector2i operator /(Vector2i a, int s)
+        {
+            return new Vector2i(a.x / s, a.y / s);
+        }
+
+        public static Vector2 operator /(Vector2i a, float s)
+        {
+            return new Vector2(a.x / s, a.y / s);
+        }
+
+        public static bool operator ==(Vector2i a, Vector2i b)
+        {
+            return (a.x == b.x && a.y == b.y);
+        }
+
+        public static bool operator !=(Vector2i a, Vector2i b)
+        {
+            return (a.x != b.x || a.y != b.y);
+        }
+
+        public override bool Equals(object obj) 
+        {
+            Vector2i v = obj as Vector2i;
+            if (v != null)
+            {
+                return (x == v.x && y == v.y);
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "(" + x + "," + y + ")";
+        }
+    }
+}
